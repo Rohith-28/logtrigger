d4665400a94c9fa4650dc6e83d701dff28a6db6c
Rohith-28 <77043117+Rohith-28@users.noreply.github.com>
2023-08-16 17:32:26 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const fs = require('fs');
+const cors = require('cors');
+app.use(cors())
+app.use(express.json())
+app.get('/api/data', (req, res) => {
+  fs.readFile('sample.json', 'utf8', (error, data) => {
+    if (error) {
+      console.error('Error reading file:', error);
+      return;
+    }
+    
+    res.json(JSON.parse(data));
+    console.log('File contents:', data);
+  });
+
+})
+
+app.post('/api/data', (req, res) => {
+    console.log("The Request body is",JSON.stringify(req.body))
+    fs.writeFile('sample.json',JSON.stringify(req.body), 'utf8', (error) => {
+        if (error) {
+          console.error('Error writing to file:', error);
+          return;
+        }
+        res.json(JSON.stringify(req.body))
+        console.log('Data written to file successfully.');
+      });
+    
+   
+  })
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
