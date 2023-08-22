e2da739bae0dbafeec851a9c605270f818b503bc
Rohith-28 <77043117+Rohith-28@users.noreply.github.com>
2023-08-22 17:50:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 app.use(cors())
 app.use(express.json())
 app.get('/api/data', (req, res) => {
-  fs.readFile('D://sample.json', 'utf8', (error, data) => {
+  fs.readFile('sample.json', 'utf8', (error, data) => {
     if (error) {
       console.error('Error reading file:', error);
       return;
